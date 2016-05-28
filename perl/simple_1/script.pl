0147aae27a27abcb3024242b42387614f42dc556
root <root@sandbox001.home>
2016-05-28 11:25:46 -0400

    moved and updated first perl script here

--- perl/simple_1/script.pl
+++ perl/simple_1/script.pl
@@ -4,17 +4,16 @@
 use strict;
 
 
-open(DATA_read, '</root/programming/perl/data') or die;
-open(DATA_write, '>>/root/programming/perl/data') or die;
+open(DATA_read, '</root/programming/perl/simple_1/data') or die;
+open(DATA_write, '>>/root/programming/perl/simple_1/data') or die;
 
 while (<DATA_read>) {
 
 	if ($_ =~ m/(\d+)/) {
-		#print "$_";
 		print DATA_write $_;
 	}
-
 }
-
+print "0\n";
 close(DATA_read);
- 
+close(DATA_write);
+
