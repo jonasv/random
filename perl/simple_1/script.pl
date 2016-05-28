07684339bd48509fc0d79cebec37c74adf370464
root <root@sandbox001.home>
2016-05-28 11:32:00 -0400

    added description for perl script functionality

--- perl/simple_1/script.pl
+++ perl/simple_1/script.pl
@@ -1,19 +1,20 @@
 #!/usr/bin/perl
 
 # In a language of your choice (not BASH), take all lines of a given file with a digit in them and move them to the end of the file.
-use strict;
+use strict; #default :P
 
 
-open(DATA_read, '</root/programming/perl/simple_1/data') or die;
-open(DATA_write, '>>/root/programming/perl/simple_1/data') or die;
+open(DATA_read, '</root/programming/perl/simple_1/data') or die; #open filehandle for reading
+open(DATA_write, '>>/root/programming/perl/simple_1/data') or die; #open filehandle for writting, because its must painless than to use +>> with seek() ....
 
-while (<DATA_read>) {
+while (<DATA_read>) { #read file..
 
-	if ($_ =~ m/(\d+)/) {
-		print DATA_write $_;
+	if ($_ =~ m/(\d+)/) { #.. then if there is a diggit
+		print DATA_write $_; # .. then append those lines with digits to the same file
 	}
 }
 print "0\n";
 close(DATA_read);
-close(DATA_write);
+close(DATA_write); 
+#finally close both filehandles and print exit status 0
 
