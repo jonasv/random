e053e714ba2ed9b211a9c1792b57f13b90cabd7c
root <root@sandbox001.home>
2016-06-08 07:42:33 +0300

    finished on perl transliteration

--- perl/moreontransliteration/script.pl
+++ perl/moreontransliteration/script.pl
@@ -4,5 +4,7 @@ use strict;
 use warnings;
 
 
-print "a b c" =~ tr/a-z/c-za-b/r . "\n";
-#   C D E
+print "a b c" =~ tr/a-z/c-z/r . "\n";
+   #   C D E        1*  2*     
+   #   		    1*all from a to z
+   #   		    2*second letter from a will be c, so all processed letters will follow this order till letter z which is last defined.
--- perl/pythonchallenge2/script2.pl
+++ perl/pythonchallenge2/script2.pl
@@ -4,7 +4,8 @@
 use strict;
 use warnings;
 
-print "koe  g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj." =~ tr/a-z/c-za-b/r;
+
+print "koe  g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj." =~ tr/a-z/c-za-b/r . "\n"; #a-b is useless in second part and used as an example
 
 
 
