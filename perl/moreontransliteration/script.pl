fb034c1739cd1ab57fa5e163cfd4da995df3d85c
Jonas Vasiliauskas <jonas.vasiliauskas@yahoo.com>
2016-06-09 01:23:40 +0300

    Update script.pl

--- perl/moreontransliteration/script.pl
+++ perl/moreontransliteration/script.pl
@@ -8,3 +8,4 @@ print "a b c" =~ tr/a-z/c-z/r . "\n";
    #   C D E        1*  2*     
    #   		    1*all from a to z
    #   		    2*second letter from a will be c, so all processed letters will follow this order till letter z which is last defined.
+   # full answer here: http://stackoverflow.com/questions/37713115/transliteration
