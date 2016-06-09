2dec685ffa0f66526dc61c6ce4d84048e588fad6
root <root@sandbox001.home>
2016-06-09 17:05:48 +0300

    challenge update

--- perl/anotherchallenge/js2perl.pl
+++ perl/anotherchallenge/js2perl.pl
@@ -21,29 +21,16 @@ use warnings;
 #		}	
 
 my @ascii = '0';
-#my $a = '0';
-#my $b = '0';
-#my $c = '0';
 my $i = '0';
-
-sub dec2bin {
-    my $str = unpack("B32", pack("N", shift));
-    $str =~ s/^0+(?=\d)//;   # otherwise you'll get leading zeros
-    return $str;
-a}
 
 sub process {
 	my ($ascii,$a,$b,$c) = @_;
 	for ($i=0;$i<length($ascii);$i++) {
 		#print "$ascii $a $b $c \n";
 		if ($i % '3' == '0') {
-			$ascii[$i] = ($ascii[$i] + $a) % '256';
-			#print "1 \n";
+			$ascii[$i] = ($ascii[$i]+$a)%256;
 		}
-	#print $ascii[$i] . "\n";
-	#printf "%b\n", $decimal;
-	#sprintf("%c", $ascii)
-	print $ascii;
+	return $ascii;
 	}
 }
 
