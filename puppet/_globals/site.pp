69dfb3f53aea0a3ce7009da0c929dfdc40e164c1
jonas.vasiliauskas <jonas.vasiliauskas@yahoo.com>
2016-08-14 15:03:39 +0300

    added globals for puppet modules

---
+++ puppet/_globals/site.pp
@@ -0,0 +1,27 @@
+
+resources { "firewall":
+          purge => true
+}
+
+Firewall {
+          before => Class['fw::post'],
+                  require => Class['fw::pre'],
+}
+
+class { ['fw::pre', 'fw::post']: }
+
+
+
+node 'puppetnode001.int.corosay.com' {
+    file { '/tmp/xxxa':
+         content => "f",
+         ensure => "present",       
+    }
+
+
+     include httpd
+     # include users1
+     # include users2
+       include accounts
+     realize (Accounts::Virtual['jsmith'])
+}
