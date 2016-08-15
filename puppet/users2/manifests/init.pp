24b751ca098275a783b447cad4453627444f7a80
jonas.vasiliauskas <jonas.vasiliauskas@yahoo.com>
2016-08-15 20:15:35 +0300

    fixed users2 module

--- puppet/users2/manifests/init.pp
+++ puppet/users2/manifests/init.pp
@@ -1,6 +1,6 @@
 class users2 {
 
-  users2::virtual { 'johndoe':
+  @users2::virtual { 'johndoe':
     	uid		=>  '1042',
 	realname        =>  'John Doe',
     	pass            =>  'wDa89lUuzLxUw', # password is set to a
