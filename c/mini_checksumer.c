986b0d8981cadde67b59d2516a82f8272e5bfa5d
jonasv <jonas.vasiliauskas@yahoo.com>
2024-07-25 02:21:37 +0100

    Update mini_checksumer.c

--- c/mini_checksumer.c
+++ c/mini_checksumer.c
@@ -1,5 +1,3 @@
-// Provide checksum of first 10mb of the file if it is larger than 10mb,
-// otherwise just provide sha256 checksum of the file
 // Needs libssl-dev/openssl-devel
 // gcc -o mini_checksumer mini_checksumer.c -lssl -lcrypto
 
@@ -15,7 +13,7 @@ void print_usage(const char* program_name) {
     fprintf(stderr, "Usage: %s <file_path>\n", program_name);
 }
 
-char* calculate_sha256_partial(FILE* file, long read_size) {
+char* calculate_sha256(FILE* file, long read_size, long offset) {
     EVP_MD_CTX *mdctx;
     const EVP_MD *md;
     unsigned char hash[EVP_MAX_MD_SIZE];
@@ -28,6 +26,8 @@ char* calculate_sha256_partial(FILE* file, long read_size) {
     char buffer[BUFFER_SIZE];
     size_t bytes_read;
     long total_read = 0;
+
+    fseek(file, offset, SEEK_SET);
 
     while (total_read < read_size && (bytes_read = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
         EVP_DigestUpdate(mdctx, buffer, bytes_read);
@@ -75,6 +75,31 @@ char* calculate_sha256_full(FILE* file) {
     return output;
 }
 
+char* combine_and_hash(const char* hash1, const char* hash2) {
+    EVP_MD_CTX *mdctx;
+    const EVP_MD *md;
+    unsigned char hash[EVP_MAX_MD_SIZE];
+    unsigned int hash_len;
+
+    md = EVP_sha256();
+    mdctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(mdctx, md, NULL);
+
+    EVP_DigestUpdate(mdctx, hash1, strlen(hash1));
+    EVP_DigestUpdate(mdctx, hash2, strlen(hash2));
+
+    EVP_DigestFinal_ex(mdctx, hash, &hash_len);
+    EVP_MD_CTX_free(mdctx);
+
+    char* output = malloc(65);
+    for(int i = 0; i < hash_len; i++) {
+        sprintf(output + (i * 2), "%02x", hash[i]);
+    }
+    output[64] = 0;
+
+    return output;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         print_usage(argv[0]);
@@ -93,8 +118,14 @@ int main(int argc, char *argv[]) {
     rewind(file);
 
     char* checksum;
-    if (file_size >= 10 * MB) {
-        checksum = calculate_sha256_partial(file, 10 * MB);
+    if (file_size > 21 * MB) {
+        char* checksum_first = calculate_sha256(file, 10 * MB, 0);
+        char* checksum_last = calculate_sha256(file, 10 * MB, file_size - 10 * MB);
+
+        checksum = combine_and_hash(checksum_first, checksum_last);
+
+        free(checksum_first);
+        free(checksum_last);
     } else {
         checksum = calculate_sha256_full(file);
     }
